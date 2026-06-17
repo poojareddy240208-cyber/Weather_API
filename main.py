db14425d5ec5f4aecaf6e804a9f3516f8ba86648
Pooja Reddy <poojareddy240208@gmail.com>
2026-06-17 15:39:46 +0530

    Migrate weather API to FastAPI

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from weather_api import get_weather
+from schemas import WeatherResponse
 
 app = FastAPI()
 
-@app.get("/weather")
+@app.get("/weather",response_model=WeatherResponse)
 def weather(city: str):
 
     data = get_weather(city)
